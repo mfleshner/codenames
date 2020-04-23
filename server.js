6a7eb40d0ddecca2bdc07961d2078cc011afb524
Matt Fleshner <mattfleshner@gmail.com>
2020-04-22 19:01:15 -0500

    array of variables stored on server

--- server.js
+++ server.js
@@ -7,14 +7,10 @@ const app = express();
 var CardModel = require('./cards');
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-var all_cards = [];
-var new_cards = [];
-var old_arr = [];
-var spymaster = 0;
-var turn = "Blue";
-var death_win = "";
-var players = 0;
-
+var database = [{all_cards: [], new_cards: [], old_arr: [], spymaster: 0, turn: "Blue", death_win: "", players: 0},
+                 {all_cards: [], new_cards: [], old_arr: [], spymaster: 0, turn: "Blue", death_win: "", players: 0},
+                 {all_cards: [], new_cards: [], old_arr: [], spymaster: 0, turn: "Blue", death_win: "", players: 0},
+                 {all_cards: [], new_cards: [], old_arr: [], spymaster: 0, turn: "Blue", death_win: "", players: 0}];
 app.use(express.static('./public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -40,37 +36,37 @@ var shuffleArray = function(array) {
 _pickCards = function(){
   var arr = [];
   while(arr.length < 25){
-    var r = Math.floor(Math.random() * (all_cards.length-1));
-    if((arr.indexOf(r) === -1) && (old_arr.indexOf(r) === -1)){ arr.push(r); old_arr.push(r);}
+    var r = Math.floor(Math.random() * (database[0].all_cards.length-1));
+    if((arr.indexOf(r) === -1) && (database[0].old_arr.indexOf(r) === -1)){ arr.push(r); database[0].old_arr.push(r);}
   }
-  if(old_arr.length > 795) old_arr = [];
+  if(database[0].old_arr.length > 795) database[0].old_arr = [];
   for(var i = 0; i < 25; i++){
-    new_cards[i] = all_cards[arr[i]];
-    new_cards[i].selected = false;
+    database[0].new_cards[i] = database[0].all_cards[arr[i]];
+    database[0].new_cards[i].selected = false;
   }
   var arr = [];
   while(arr.length < 25){
-    var r = Math.floor(Math.random() * (new_cards.length));
+    var r = Math.floor(Math.random() * (database[0].new_cards.length));
     if(arr.indexOf(r) === -1) arr.push(r);
   }
   for(var i = 0; i < 9; i++){
-    new_cards[arr[i]].textColor = "blue";
-    new_cards[arr[i]].backColor = "backblue";
+    database[0].new_cards[arr[i]].textColor = "blue";
+    database[0].new_cards[arr[i]].backColor = "backblue";
   }
   for(var i = 9; i < 17; i++){
-    new_cards[arr[i]].textColor = "red";
-    new_cards[arr[i]].backColor = "backred";
+    database[0].new_cards[arr[i]].textColor = "red";
+    database[0].new_cards[arr[i]].backColor = "backred";
   }
   for(var i = 17; i < 24; i++){
-    new_cards[arr[i]].textColor = "yellow";
-    new_cards[arr[i]].backColor = "backyellow";
+    database[0].new_cards[arr[i]].textColor = "yellow";
+    database[0].new_cards[arr[i]].backColor = "backyellow";
   }
-  new_cards[arr[24]].textColor = "black";
-  new_cards[arr[24]].backColor = "backblack";
+  database[0].new_cards[arr[24]].textColor = "black";
+  database[0].new_cards[arr[24]].backColor = "backblack";
 }
 
 _shuffle = function(){
-  shuffleArray(all_cards);
+  shuffleArray(database[0].all_cards);
   _pickCards();
   console.log("server: 25 new cards!");
 }
@@ -80,9 +76,9 @@ _newCards = function(){
   CardModel.find({}).lean().exec(function (err, cards) {
     if (err) console.log('Error getting cards:', err);
     else {
-      all_cards = cards;
-      if(all_cards.length > 1) _shuffle();
-      spymaster = 0; //reset spymaster
+      database[0].all_cards = cards;
+      if(database[0].all_cards.length > 1) _shuffle();
+      database[0].spymaster = 0; //reset spymaster
     }
   });
 }
@@ -106,41 +102,41 @@ http.listen(8090, () => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
-  players++;
-  io.emit('spymaster', spymaster); //send spymaster count
-  io.emit('get cards', new_cards); //send current cards
-  io.emit('turn', turn); //send current turn
-  io.emit('players', players); //send player count
+  database[0].players++;
+  io.emit('spymaster', database[0].spymaster); //send spymaster count
+  io.emit('get cards', database[0].new_cards); //send current cards
+  io.emit('turn', database[0].turn); //send current turn
+  io.emit('players', database[0].players); //send player count
   socket.on('disconnect', () => {
     console.log('user disconnected');
-    players--;
-    io.emit('players', players); //send player count
+    database[0].players--;
+    io.emit('players', database[0].players); //send player count
   });
   socket.on('card selected', (card) => {
-    if(!new_cards[card].selected) new_cards[card].selected = true;
-    else new_cards[card].selected = false;
-    io.emit('card selected', new_cards);
+    if(!database[0].new_cards[card].selected) database[0].new_cards[card].selected = true;
+    else database[0].new_cards[card].selected = false;
+    io.emit('card selected', database[0].new_cards);
   });
   socket.on('spymaster', (spy) => {
-    if(spy) spymaster++;
-    else if(spymaster > 0) spymaster--;
-    io.emit('spymaster', spymaster);
+    if(spy) database[0].spymaster++;
+    else if(database[0].spymaster > 0) database[0].spymaster--;
+    io.emit('spymaster', database[0].spymaster);
   });
   socket.on('turn', (whoTurn) => {
-    turn = whoTurn;
-    io.emit('turn', turn);
+    database[0].turn = whoTurn;
+    io.emit('turn', database[0].turn);
   });
   socket.on('death card', (death) => {
-    death_win = death;
-    io.emit('death card', death_win);
+    database[0].death_win = death;
+    io.emit('death card', database[0].death_win);
   });
   socket.on('shuffle', () => {
     _pickCards(); //don't reshuffle, just pick new cards
-    io.emit('next game', new_cards);
+    io.emit('next game', database[0].new_cards);
   });
   socket.on('reset', () => {
-    spymaster = 0;
-    turn = "Blue";
+    database[0].spymaster = 0;
+    database[0].turn = "Blue";
     io.emit('spymaster reset');
   });
 });
